0f3c25be624f34016c21c1b5f2f2ad12ff4b7055
GenaEphirov <107998074+GenaEphirov@users.noreply.github.com>
2022-09-25 10:24:30 +0300

    draft nav bar

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+const minSquare = document.getElementById('minimum-square');
+const dropdownMenu = document.getElementById('nav-dropdown');
+
+minSquare.addEventListener('click', navDropdownEnable);
+
+function navDropdownEnable() {
+  let dropdownMenuStyle = dropdownMenu.style;
+  dropdownMenuStyle.left = '0px';
+  dropdownMenuStyle.opacity = '1';
+
+  let minSquareStyle = minSquare.style;
+  minSquareStyle.left = '0px';
+  minSquareStyle.backgroundColor = 'tomato';
+  minSquareStyle.opacity = '1';
+
+  minSquare.removeEventListener('click', navDropdownEnable);
+  minSquare.addEventListener('click', navDropdownDisable);
+}
+
+function navDropdownDisable() {
+  console.log('disabled');
+}
